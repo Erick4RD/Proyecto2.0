b4073b72268f785c06bc95e4129c5fcc04a7abf6
Erick4RD <60790548+Erick4RD@users.noreply.github.com>
2020-02-08 12:39:13 -0400

    primer commit

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyApplication.Models;
+
+namespace EasyApplication.Data
+{
+    public class DbInitializer
+    {
+
+        //busqueda de registro para materias //
+        public static void Initialize (EasyApplicationContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if(context.Categoria.Any())
+            {
+
+                return;
+
+            }
+
+            var categoria = new Categoria[]
+            {
+                //pruebas x2
+
+                new Categoria{Nombre="Programacion",Descripcion="Materia de Programacion",Estado= true},
+                 new Categoria{Nombre="Diseño grafico",Descripcion="Materia de Diseño grafico",Estado= true}
+            };
+
+            foreach(Categoria c in categoria)
+            {
+                context.Categoria.Add(c);
+
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ Data/EasyApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EasyApplication.Models;
+
+namespace EasyApplication.Data
+{
+    public class EasyApplicationContext : DbContext
+    {
+        public EasyApplicationContext (DbContextOptions<EasyApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EasyApplication.Models.Categoria> Categoria { get; set; }
+    }
+}
